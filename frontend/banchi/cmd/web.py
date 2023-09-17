5f2ca713c5a3c0e26501dbe11783401972afb592
Thanathip Limna <boatkrap@gmail.com>
2023-09-17 16:20:45 +0700

    fix for run on web

--- frontend/banchi/cmd/web.py
+++ frontend/banchi/cmd/web.py
@@ -1,9 +1,18 @@
-import flet as ft
+import flet
+import os
 
 
-async def main(page: ft.Page):
-    await page.add_async(ft.Text("Hello, async world!"))
+DEFAULT_FLET_PATH = ""  # or 'ui/path'
+DEFAULT_FLET_PORT = 8550
+
+
+async def main(page: flet.Page):
+    await page.add_async(flet.Text("Hello, async world! xxxx"))
 
 
 if __name__ == "__main__":
-    ft.app(main)
+    flet_path = os.getenv("FLET_PATH", DEFAULT_FLET_PATH)
+    flet_port = int(os.getenv("FLET_PORT", DEFAULT_FLET_PORT))
+    print("FLET_PATH:", flet_path)
+    print("FLET_PORT:", flet_port)
+    flet.app(name=flet_path, target=main, view=flet.WEB_BROWSER, port=flet_port)
