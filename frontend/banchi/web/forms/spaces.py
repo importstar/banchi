3318621d6e1c86c78d0438eb657f29d9fbd9c174
Thanathip Limna <boatkrap@gmail.com>
2023-10-28 21:43:38 +0700

    add new space

---
+++ frontend/banchi/web/forms/spaces.py
@@ -0,0 +1,19 @@
+"""
+Created on Oct 13, 2013
+
+@author: boatkrap
+"""
+
+from wtforms import validators
+from wtforms import fields
+
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed
+
+from .fields import TagListField
+
+
+class SpaceForm(FlaskForm):
+    name = fields.StringField(
+        "Name", validators=[validators.InputRequired(), validators.Length(min=3)]
+    )
