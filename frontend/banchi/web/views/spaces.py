cc36b6063e2ebfb047c2634497adba0bc43a80e6
Thanathip Limna <boatkrap@gmail.com>
2023-10-29 00:01:00 +0700

    add space

--- frontend/banchi/web/views/spaces.py
+++ frontend/banchi/web/views/spaces.py
@@ -7,3 +7,9 @@ module = Blueprint("spaces", __name__, url_prefix="/spaces")
 @module.route("")
 def index():
     return render_template("/spaces/index.html")
+
+
+@module.route("/create", defaults=dict(space_id=None), methods=["GET", "POST"])
+@module.route("/<space_id>/edit", methods=["GET", "POST"])
+def create_or_edit(space_id):
+    return render_template("/spaces/index.html")
