e5427ac5797affd01894ccb333ac1e77a6c72fab
Thanathip Limna <boatkrap@gmail.com>
2023-12-03 21:03:16 +0700

    fix username

--- banchiapi/scripts/init_admin.py
+++ banchiapi/scripts/init_admin.py
@@ -1,43 +1,42 @@
 import sys
-import mongoengine as me
 
-# import pandas as pd
 from banchiapi import models
 import datetime
+import asyncio
 
 
-def create_user_admin():
+async def create_user_admin():
+    class Setting:
+        def __init__(self):
+            self.MONGODB_URI = "mongodb://localhost/banchidb"
+
+    settings = Setting()
+    if len(sys.argv) > 1:
+        settings.MONGODB_URI = "mongodb://mongodb/banchidb"
+
+    await models.init_beanie(None, settings)
+
+    print("start check admin")
+    user = await models.users.User.find_one(models.users.User.username == "admin")
+
+    if user:
+        print("Found admin user", user)
+        return
+    print("end check admin")
+
     print("start create admin")
-    space = models.Space(
-        name="admin", slogan="", tax_id="0905564002794", code="000", phone="0819693002"
-    )
-    space.save()
-    division = models.Division(name="admin", code="000", space=space)
-    division.save()
-    user = models.User(
+    user = models.users.User(
         email="admin@example.com",
         username="admin",
-        title_name="admin",
+        password="",
         first_name="admin",
         last_name="system",
-        citizen_id="0000000000000",
         roles=["user", "admin"],
-        birthday=datetime.datetime.now(),
-        division=division,
-        space=space,
     )
-
-    user.set_password("p@ssw0rd")
-    user.save()
+    await user.set_password("p@ssw0rd")
+    await user.save()
     print("finish")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        me.connect(db="banchaidb", host=sys.argv[1])
-    else:
-        me.connect(db="banchaidb")
-    print("start check")
-    create_user_admin()
-
-    print("end check")
+    asyncio.run(create_user_admin())
