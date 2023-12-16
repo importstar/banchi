a6585dc30a842a9083a0ae0ac713a89311f8d4c4
Thanathip Limna <boatkrap@gmail.com>
2023-12-16 23:23:17 +0700

    fix lib name

--- frontend/banchi/web/utils/account_books.py
+++ frontend/banchi/web/utils/account_books.py
@@ -1,19 +1,23 @@
 def get_display_names(account_books):
-    account_book_display_names = {
-        account_book.id: account_book.name for account_book in account_books
+    account_book_dict = {
+        account_book.id: account_book for account_book in account_books
     }
 
+    account_book_display_names = {}
     parents = dict()
 
     for account_book in account_books:
         if account_book.parent:
             parents[account_book.id] = account_book.parent.id
 
+    # print(account_book_dict)
     for account_book in account_books:
         account_book_id = account_book.id
+
+        account_book_display_names[account_book.id] = account_book.name
         while account_book_id in parents:
             account_book_display_names[account_book.id] = "{} > {}".format(
-                account_book_display_names[parents[account_book_id]],
+                account_book_dict[parents[account_book_id]].name,
                 account_book_display_names[account_book.id],
             )
             account_book_id = parents[account_book_id]
